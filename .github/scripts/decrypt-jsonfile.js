8c2ed694083346c8e082fbe6b3b2586b41817cc7
Thi <67835317+dinhdobathi1992@users.noreply.github.com>
2024-03-21 17:47:33 +0700

    Update decrypt-jsonfile.js

--- .github/scripts/decrypt-jsonfile.js
+++ .github/scripts/decrypt-jsonfile.js
@@ -4,7 +4,8 @@ const NodeRSA = require("node-rsa");
 // Load public and private keys
 const publicKey = fs.readFileSync('public.pem', 'utf8');
 const privateKey = fs.readFileSync('private.pem', 'utf8');
-
+const ENV = process.argv[2];
+const FILE = process.argv[3];
 // Create RSA instances with keys
 const publicKeyInstance = new NodeRSA(publicKey);
 const privateKeyInstance = new NodeRSA(privateKey);
@@ -13,10 +14,10 @@ const privateKeyInstance = new NodeRSA(privateKey);
 // //const jsonData = JSON.parse(fs.readFileSync('example-env.json', 'utf8'));
 
 // // Decrypt data from the encrypted file
-const encryptedDataFromFile = fs.readFileSync('aws/example/encrypted_data.txt', 'utf8');
+const encryptedDataFromFile = fs.readFileSync(FILE, 'utf8');
 const decryptedData = privateKeyInstance.decrypt(encryptedDataFromFile, 'utf8');
-
+const destination = `aws/${ENV}/decrypted_data.json`;
 // // Save decrypted data to another file
-fs.writeFileSync('aws/example/decrypted_data.json', decryptedData, 'utf8');
+fs.writeFileSync(destination, decryptedData, 'utf8');
 
 console.log('Data decrypted and saved to decrypted_data.json');
